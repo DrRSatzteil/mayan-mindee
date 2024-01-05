6b7d4423d13dc9d056cc0386c60a73568911ed4e
Thomas Lauterbach <2452988+DrRSatzteil@users.noreply.github.com>
2024-01-05 22:55:08 +0100

    mindeeapi.py aktualisieren
    More api changes

--- mayanmindee/mindeeapi.py
+++ mayanmindee/mindeeapi.py
@@ -7,8 +7,8 @@ __all__ = []
 
 def ocr_standard(pdf_bytes, document_name, document_type):
     client = Client(api_key=os.getenv("MINDEE_API_KEY"))
-    doc = client.doc_from_bytes(pdf_bytes, document_name)
-    parsed_doc = doc.parse(getattr(product, document_type))
+    doc = client.source_from_bytes(pdf_bytes, document_name)
+    parsed_doc = client.parse(getattr(product, document_type), doc)
     return parsed_doc
 
 
@@ -17,6 +17,6 @@ def ocr_custom(pdf_bytes, document_name, account_name, endpoint_name):
         account_name=account_name,
         endpoint_name=endpoint_name,
     )
-    doc = client.doc_from_bytes(pdf_bytes, document_name)
+    doc = client.source_from_bytes(pdf_bytes, document_name)
     parsed_doc = doc.parse(product.TypeCustomV1, endpoint_name=endpoint_name)
     return parsed_doc
