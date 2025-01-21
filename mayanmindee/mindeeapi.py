0e1041776e9d9115cc77643d167b9afa692b6f53
Thomas Lauterbach <thomas@pop-os.amfthome.org>
2025-01-21 23:40:35 +0100

    secret support for custom apis
    Signed-off-by: Thomas Lauterbach <thomas@pop-os.amfthome.org>

--- mayanmindee/mindeeapi.py
+++ mayanmindee/mindeeapi.py
@@ -17,7 +17,11 @@ def ocr_standard(pdf_bytes, document_name, document_type):
 
 
 def ocr_custom(pdf_bytes, document_name, account_name, endpoint_name, model_version, synchronous=False):
-    client = Client(api_key=os.getenv("MINDEE_API_KEY"))
+    api_key = os.getenv("MINDEE_API_KEY");
+    if not apikey:
+        with open(os.getenv("MINDEE_API_KEY_FILE"), "r") as file:
+            apikey = file.read().rstrip()
+    client = Client(api_key=api_key)
     custom_endpoint = client.create_endpoint(
         account_name=account_name,
         endpoint_name=endpoint_name,
